8efcf21cea598d6117529c4f560f07ca741f2ce8
aish5612 <106028537+aish5612@users.noreply.github.com>
2022-09-21 02:44:06 +0530

    Add files via upload

---
+++ lcm_hcf.py
@@ -0,0 +1,28 @@
+def lcm(x, y):
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+def hcf(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
+
+num1 = int(input())
+num2 = int(input())
+print("hcf: ", hcf(num1, num2))
+print("lcm: ", lcm(num1, num2))
